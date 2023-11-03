167aabcdd986c860ba18da47877cb5aafc3cfc57
Fikret Aslan <f.aslan0191@gmail.com>
2023-11-03 13:33:56 +0000

    changes to backend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(cors());
+
+const data = require("./data/weather.json");
+
+app.get("/weather", (request, response) => {
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  const searchQuery = request.query.searchQuery;
+
+  const filteredCity = data.find((city) => {
+    return (
+      city.city_name === searchQuery && city.lat === lat && city.lon === lon
+    );
+  });
+  const processedData = filteredCity.data.map((day) => {
+    return {
+      description: day.weather.description,
+      date: day.datetime,
+    };
+  });
+  response.json(processedData);
+});
+app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
